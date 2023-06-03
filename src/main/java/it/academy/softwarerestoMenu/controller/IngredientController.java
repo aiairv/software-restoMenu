45bc0218984d0b77be148cf8d0395f33c2192ebc
Gulkaiyr_Bakytbekova <bakytbekova6869@gmail.com>
2023-06-03 23:34:28 +0600

    added responseMessage for ingredient

--- src/main/java/it/academy/softwarerestoMenu/controller/IngredientController.java
+++ src/main/java/it/academy/softwarerestoMenu/controller/IngredientController.java
@@ -1,34 +1,81 @@
 package it.academy.softwarerestoMenu.controller;
 
+import it.academy.softwarerestoMenu.entity.Category;
+import it.academy.softwarerestoMenu.entity.ResponseMessage;
+import it.academy.softwarerestoMenu.enums.ResultCode;
 import it.academy.softwarerestoMenu.exceptions.CategoryNotFoundException;
 import it.academy.softwarerestoMenu.entity.Ingredient;
+import it.academy.softwarerestoMenu.exceptions.IngredientNotFoundException;
 import it.academy.softwarerestoMenu.services.IngredientService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@Slf4j
+@AllArgsConstructor
 @RequestMapping("/ingredients")
 public class IngredientController {
 
     private final IngredientService service;
 
-    public IngredientController(IngredientService service) {
-        this.service = service;
-    }
-
     @PostMapping("/")
     @ResponseStatus(HttpStatus.CREATED)
-    public Ingredient create(@RequestBody Ingredient ingredient) {
-        return service.create(ingredient);
+    public ResponseMessage<Ingredient> create(@RequestBody Ingredient ingredient) {
+        try {
+            return new ResponseMessage<>(
+                    service.create(ingredient),
+                    ResultCode.SUCCESS,
+                    "Ингредиент успешно создан",
+                    ResultCode.SUCCESS.getHttpCode()
+            );
+        } catch (Exception exception) {
+            log.error("IngredientController: create", exception);
+            return new ResponseMessage<>(
+                    null,
+                    ResultCode.FAIL,
+                    exception.getMessage(),
+                    ResultCode.FAIL.getHttpCode()
+            );
+        }
     }
 
     @GetMapping("/{id}")
-    public Ingredient getById(@PathVariable Long id)  {
-        return service.getById(id);
+    public ResponseMessage<Ingredient> getById(@PathVariable Long id) {
+        try {
+            return new ResponseMessage<>(
+                    service.getById(id),
+                    ResultCode.SUCCESS,
+                    "Ингредиент по ID найден",
+                    ResultCode.SUCCESS.getHttpCode()
+            );
+        } catch (Exception exception) {
+            log.error("IngredientController: getById", exception);
+            return new ResponseMessage<>(
+                    null,
+                    ResultCode.FAIL,
+                    exception.getMessage(),
+                    ResultCode.FAIL.getHttpCode()
+            );
+        }
     }
 
+    @PostMapping("/{id}/restore")
+    public ResponseMessage<Ingredient> restore(@PathVariable Long id) {
+        try {
+            return new ResponseMessage<>(
+                    service.restore(id),
+                    ResultCode.SUCCESS,
+                    "Ингредиент успешно восстановлен!",
+                    ResultCode.SUCCESS.getHttpCode());
+        } catch (Exception e) {
+            log.error("IngredientController: restore ", e);
+            return new ResponseMessage<>(null, ResultCode.FAIL, e.getMessage(), ResultCode.FAIL.getHttpCode());
+        }
+    }
 
     @GetMapping("/")
     public List<Ingredient> getAll() {
@@ -36,14 +83,30 @@ public class IngredientController {
     }
 
     @PutMapping("/")
-    public Ingredient update(@RequestBody Ingredient ingredient) {
-        if (ingredient.getId() == null) throw new CategoryNotFoundException("IngredientController: update()  id is null");
-        return service.create(ingredient);
+    public ResponseMessage<Ingredient> update(@RequestBody Ingredient ingredient) {
+        if (ingredient.getId() == null)
+            throw new IngredientNotFoundException("IngredientController: update()  id is null");
+        try {
+            return new ResponseMessage<>(
+                    service.create(ingredient),
+                    ResultCode.SUCCESS, "Ингредиент успешно обновлен!",
+                    ResultCode.SUCCESS.getHttpCode());
+        } catch (Exception exception) {
+            log.error("IngredientController: update", exception);
+            return new ResponseMessage<>(null, ResultCode.FAIL, exception.getMessage(), ResultCode.FAIL.getHttpCode());
+        }
     }
 
     @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Long id) {
-        service.delete(id);
+    public ResponseMessage<Long> delete(@PathVariable Long id) {
+        try {
+            return new ResponseMessage<>(
+                    service.delete(id),
+                    ResultCode.SUCCESS, "Ингредиент успешно удален!",
+                    ResultCode.SUCCESS.getHttpCode());
+        } catch (Exception e) {
+            log.error("IngredientController: delete ", e);
+            return new ResponseMessage<>(null, ResultCode.FAIL, e.getMessage(), ResultCode.FAIL.getHttpCode());
+        }
     }
 }
