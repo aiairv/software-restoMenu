e842ce99021a89e372e2c855e8def337f2417c9a
aiairv <a.a.kubatova@gmail.com>
2023-06-05 20:42:54 +0600

    Changed methods for Cart

--- src/main/java/it/academy/softwarerestoMenu/config/SwaggerConfig.java
+++ src/main/java/it/academy/softwarerestoMenu/config/SwaggerConfig.java
@@ -9,30 +9,6 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
-//package it.academy.softwarerestoMenu.config;//package it.academy.softwarerestoMenu.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
-//
-//@Configuration
-//@EnableWebMvc
-//@EnableSwagger2WebMvc
-//public class SwaggerConfig {
-//    @Bean
-//    public Docket api() { // http://localhost:9090/swagger-ui/index.html
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("it.academy.softwarerestoMenu"))
-//                .paths(PathSelectors.any())
-//                .build();
-//    }
-//}
 @Configuration
 @EnableWebMvc
 @EnableSwagger2WebMvc
@@ -41,7 +17,7 @@ public class SwaggerConfig {
     public Docket api() { // http://localhost:9090/swagger-ui/index.html
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("it.academy.softwarerestoMenu.controllers"))
+                .apis(RequestHandlerSelectors.basePackage("it.academy.softwarerestoMenu.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
